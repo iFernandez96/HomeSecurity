f17e29ef15e16112b861b0b386c0942704315e0b
Israel Fernandez <israelfernandez96@gmail.com>
2025-01-04 11:19:24 -0800

    Added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 
-<h1>Hello from the NANO!</h1>
+<h1 class="text-3xl fond-bold underline">Hello from the NANO!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
